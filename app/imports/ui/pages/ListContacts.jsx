1879bf0bebb365cc8500e11695eb48011ce7b736
Tyler Chinen <tlc852@hawaii.edu>
2019-03-24 23:59:22 -1000

    notes-2 finished in 42 minutes

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { Contacts } from '/imports/api/contact/contact';
+import { Notes } from '/imports/api/note/note';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Contact from '/imports/ui/components/Contact';
@@ -20,7 +21,9 @@ class ListContacts extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>List Contacts</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <Contact key={index}
+                         contact={contact}
+                         notes={this.props.notes.filter(note => (note.contactId === contact._id))}/>)}
           </Card.Group>
         </Container>
     );
@@ -30,6 +33,7 @@ class ListContacts extends React.Component {
 /** Require an array of Stuff documents in the props. */
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
+  notes: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -37,8 +41,10 @@ ListContacts.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Contacts');
+  const subscription2 = Meteor.subscribe('Notes');
   return {
     contacts: Contacts.find({}).fetch(),
-    ready: subscription.ready(),
+    notes: Notes.find({}).fetch(),
+    ready: (subscription.ready() && subscription2.ready()),
   };
 })(ListContacts);
