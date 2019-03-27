8d7a7c66989d166972398505f1f54860dc2e26d0
Tyler Chinen <tlc852@hawaii.edu>
2019-03-27 12:30:06 -1000

    delete contact implemented

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Card, Image, Feed } from 'semantic-ui-react';
+import { Card, Image, Feed, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import Note from '/imports/ui/components/Note';
 import AddNote from '/imports/ui/components/AddNote';
+import { Contacts } from '/imports/api/contact/contact';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 /** Renders a single row in the List Contact table. See pages/ListContact.jsx. */
 class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Delete succeeded' });
+    }
+  }
+
+  onClick() {
+    Contacts.remove(this.props.contact._id, this.deleteCallback);
+  }
+
   render() {
     return (
         <Card centered>
@@ -28,6 +48,9 @@ class Contact extends React.Component {
           </Card.Content>
           <Card.Content extra>
             <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+          <Card.Content extra>
+            <Button basic onClick={this.onClick}>Delete</Button>
           </Card.Content>
           </Card>
     );
